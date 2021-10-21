9505c7f6afeb36ad6931ef1a8708bf9c9311166c
Lukas <lamm@ifam.rwth-aachen.de>
2021-10-21 15:54:17 +0200

    Added GameFactory

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
-#include "RegularCovexPolygon.h"
-#include "Rule.h"
-#include "UniqueRule.h"
-#include "NoNeighborRule.h"
-#include "NeighborRule.h"
+#include "GameFactory.h"
 #include "Game.h"
 #include <memory>
 
+/*
+*   Command line call:
+*   
+*   chagapy                                  -> Default model (3 vertices, Std.-Rule, 0.5 jump)
+*   chagapy GAME_NAME                        -> (3 vertices, 0.5 jump, Rule determined by GAME_NAME)
+*   chagapy GAME_NAME NUM_VERTICES           -> (NUM_VERTICES vertices, 0.5 jump, Rule determined by GAME_NAME)
+*   chagapy GAME_NAME NUM_VERTICES JUMP_SIZE -> (NUM_VERTICES vertices, JUMP_SIZE jump, Rule determined by GAME_NAME)
+*
+*/
 int main(int argc, char **argv)
 {
-    int num_vertices = 3;
-    std::shared_ptr<RegularConvexPolygon> base = std::make_shared<RegularConvexPolygon>(num_vertices, 1);
-    std::shared_ptr<NoNeighborRule> rule = std::make_shared<NoNeighborRule>(num_vertices);
-    Game game(100000, base, rule);
+    int num_vertices = 5;
+    unsigned int num_rounds = 100000;
+    GameFactory factory("Polygon_UniqueRule", num_rounds);
+    Game game = factory.create(num_vertices);
     game.run();
     export_csv("test.csv", game);
 }
