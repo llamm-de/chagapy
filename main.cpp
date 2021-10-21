fa3b56a55d54e9d975fd0a4c110a50db267ac651
Lukas <lamm.lukas@gmail.com>
2021-10-21 17:46:19 +0200

    Intermediate commit

--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@
 
 int main(int argc, char **argv)
 {
-    int num_vertices = 6;
+    int num_vertices = 7;
+    unsigned int num_rounds = 1000000;
+
     std::shared_ptr<RegularConvexPolygon> base = std::make_shared<RegularConvexPolygon>(num_vertices, 1);
-    std::shared_ptr<NoNeighborRule> rule = std::make_shared<NoNeighborRule>(num_vertices);
-    Game game(100000, base, rule);
+    std::shared_ptr<NoNeighborRule> rule = std::make_shared<NoNeighborRule>(num_vertices, 0.5f);
+    Game game(num_rounds, base, rule);
     game.run();
     export_csv("test.csv", game);
 }
