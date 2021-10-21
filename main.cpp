05c18195ae9e9ebf9b6f8629d432a56e36a047c9
Lukas <lamm@ifam.rwth-aachen.de>
2021-10-21 12:08:18 +0200

    Added NeighborRule

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include "Rule.h"
 #include "UniqueRule.h"
 #include "NoNeighborRule.h"
+#include "NeighborRule.h"
 #include "Game.h"
 #include <memory>
 
 int main(int argc, char **argv)
 {
-    int num_vertices = 6;
+    int num_vertices = 3;
     std::shared_ptr<RegularConvexPolygon> base = std::make_shared<RegularConvexPolygon>(num_vertices, 1);
     std::shared_ptr<NoNeighborRule> rule = std::make_shared<NoNeighborRule>(num_vertices);
     Game game(100000, base, rule);
